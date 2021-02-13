a0fb2cf30f70e8128a77ee5be5a11be8c469118d
Mehedi Hasan <mehedihasanmax95@gmail.com>
2021-02-14 00:02:14 +0600

    Initial some Calculation added

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+console.log("SUCCESS....");
+
+//TODO: Initial value added for BDtk.We have to do calculation for all currency1.Ex: rupee2tk,rupee2rupee,rupee2dollar so on...
+function first() {
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+    output.value = 0;
+    //This for BDTK
+    if (document.getElementById("convert1").value == "bdtk") {
+        if (document.getElementById("convert2").value == "bdtk1") {
+            output.value = input.value * 1;
+        }
+    }
+    if (document.getElementById("convert1").value == "bdtk") {
+        if (document.getElementById("convert2").value == "rupee1") {
+            output.value = input.value * 0.86;
+        }
+    } 
+    if (document.getElementById("convert1").value == "bdtk") {
+        if (document.getElementById("convert2").value == "dollar1") {
+            output.value = input.value * 0.0097;
+        }
+    }
+    //TODO: this for rupee
+}
