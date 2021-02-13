50aec91854cff778abde9de117e4b6b7d5912f99
Mehedi Hasan <mehedihasanmax95@gmail.com>
2021-02-14 00:32:53 +0600

    Initial some Calculation added

--- script.js
+++ script.js
@@ -5,21 +5,36 @@ function first() {
     var input = document.getElementById("input");
     var output = document.getElementById("output");
     output.value = 0;
-    //This for BDTK
-    if (document.getElementById("convert1").value == "bdtk") {
-        if (document.getElementById("convert2").value == "bdtk1") {
+    //This for BDTKs
+    if (document.getElementById("convert1").value == "bdtk1") {
+        if (document.getElementById("convert2").value == "bdtk2") {
             output.value = input.value * 1;
         }
     }
-    if (document.getElementById("convert1").value == "bdtk") {
-        if (document.getElementById("convert2").value == "rupee1") {
+    if (document.getElementById("convert1").value == "bdtk1") {
+        if (document.getElementById("convert2").value == "rupee2") {
             output.value = input.value * 0.86;
         }
     } 
-    if (document.getElementById("convert1").value == "bdtk") {
-        if (document.getElementById("convert2").value == "dollar1") {
-            output.value = input.value * 0.0097;
+    if (document.getElementById("convert1").value == "bdtk1") {
+        if (document.getElementById("convert2").value == "dollar2") {
+            output.value = input.value * 0.012;
         }
     }
-    //TODO: this for rupee
+    //This for Rupees
+    if(document.getElementById('convert1').value == "rupee1"){
+        if (document.getElementById("convert2").value == "rupee2") {
+            output.value = input.value * 1;
+        }
+    }
+    if(document.getElementById('convert1').value == "rupee1"){
+        if (document.getElementById("convert2").value == "bdtk2") {
+            output.value = input.value * 1.17;
+        }
+    }
+    if(document.getElementById('convert1').value == "rupee1"){
+        if (document.getElementById("convert2").value == "dollar2") {
+            output.value = input.value * 0.015;
+        }
+    }
 }
